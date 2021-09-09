acc32d94f57dc5d839f6ffa67d5db4f64aab5a75
brookehiggins <brooke@cantforget.me>
2021-09-09 12:03:43 -0600

    triangle excercise

---
+++ triangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# Brooke Higgins
+# Intro to Programming (Python)
+# 09/09/2021
+# Traingle Program
+def main():
+    print("   x")
+    print("  xxx")
+    print(" xxxxx")
+    print("xxxxxxx")
+ 
+main()
